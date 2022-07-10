1a89b93ff0a45e301854c80f4a68bc1cc1fde70c
deveckm <sinygsk>
2022-07-10 19:04:37 +0900

    Commint First

--- lol/src/main/java/com/lol/analysis/controller/MemberController.java
+++ lol/src/main/java/com/lol/analysis/controller/MemberController.java
@@ -5,23 +5,35 @@ import java.util.Optional;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.lol.analysis.repository.BoardRepository;
 import com.lol.analysis.repository.MemberRepository;
+import com.lol.analysis.vo.BoardVo;
 import com.lol.analysis.vo.MemberVo;
+//import com.shop.spring_study.vo.ItemVo;
 
 @Controller
 @RequestMapping("/member")
 public class MemberController {
 	@Autowired
 	MemberRepository mr;
+	
+	@Autowired
+	BoardRepository br;
 	
 	@GetMapping("/list.do")
 	public String list(Model model) {
@@ -81,6 +93,30 @@ public class MemberController {
             return "redirect:/member/signup.do";
         }
     }
+	
+	@GetMapping("/list/{page}")
+	public String pageableList( @PathVariable int page,
+								@RequestParam(defaultValue = "postTime") String sort,
+								@RequestParam(defaultValue = "desc") String desc ,
+								Model model) {
+		int size=5;
+		Pageable pageable=null;
+		if(desc.equals("desc")) {
+			pageable=PageRequest.of(page-1, size, Sort.by(sort).descending()); //mysql limit(start,size)			
+		}else if(desc.equals("asc")) {
+			pageable=PageRequest.of(page-1, size, Sort.by(sort).ascending()); //mysql limit(start,size)			
+		}
+		//모든 jpa 함수에 아무런 제약없이 Pageable를 매개변수로 정의할 수 있고
+		//Iteralbe을 부모로 하고 totalPage nextPage total과 같이 page에 필요한 정보를 수집하는 필드를 갖는 Page를 Return 받을 수 있다.
+		
+		Page<BoardVo> itemList=br.findAll(pageable);
+		
+		
+		
+		model.addAttribute("memberList", itemList);
+		return "/member/pageableList";
+	}
+	
 	@GetMapping("/ajax/findId/{id}")
     public @ResponseBody Optional<MemberVo> findId(@PathVariable String id) {
         return mr.findById(id);
---
+++ lol/src/main/java/com/lol/analysis/repository/BoardRepository.java
@@ -0,0 +1,20 @@
+package com.lol.analysis.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.lol.analysis.vo.BoardVo;
+
+
+public interface BoardRepository extends CrudRepository<BoardVo, Integer>{
+	//멤버리스트 출력	
+	public Page<BoardVo> findAll(Pageable pageable);
+	
+	
+
+}
